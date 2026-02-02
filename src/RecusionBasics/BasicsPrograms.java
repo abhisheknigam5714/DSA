3d3b59b7b3fbc1d1f65b2574f1cb81c97662210a
abhisheknigam5714 <an5714170@gmail.com>
2026-02-02 19:46:19 +0530

    added new files

--- src/RecusionBasics/BasicsPrograms.java
+++ src/RecusionBasics/BasicsPrograms.java
@@ -1,6 +1,25 @@
 package RecusionBasics;
 
 public class BasicsPrograms {
+	  public static void  reverse(int[] arr,int i,int j){
+	       if(i>=j){
+	           return  ;
+	       }
+	       else{
+	           int temp=arr[i];
+	           arr[i]=arr[j];
+	           arr[j]=temp;
+	            reverse(arr,i+1,j-1);
+	       } 
+	   }
+	 public static int fact(int n){
+	        if(n==1){
+	            return 1;
+	        }
+	        else return n*fact(n-1);
+	    }
+	  
+	
 	public static int sum(int num){
         if(num==0){
             return 0;
@@ -10,5 +29,15 @@ public class BasicsPrograms {
 	public static void main(String[] args) {
 		
 	 System.out.println(sum(4));
+	  System.out.println(fact(4));
+	  
+	  
+	  int arr[]= {8,5,2,1};
+      reverse(arr,0,arr.length-1);
+   for(int i=0;i<arr.length;i++){
+       System.out.print(arr[i]+" ");
+   }
+    
+  }
 	 }
-}
+
